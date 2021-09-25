ac38a8b98f980c849e6620003372adf4faaea8fb
ごぼう <87304207+gobou79@users.noreply.github.com>
2021-09-25 19:38:44 +0900

    Update 189C.py

--- ABC/ABC189/189C.py
+++ ABC/ABC189/189C.py
@@ -1,25 +1,13 @@
 N = int(input())
-a = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-m = min(a) * len(a)
+ans = A[0]
 
-ind = [a.index(min(a))]
+for i in range(N):
+    x = A[i]
+    for j in range(i, N):
+        x = min(x, A[j])
+        y = x * (j-i+1)
+        ans = max(y, ans)
 
-k = 1
-
-b = []
-c = []
-if ind[0] != 0:
-    b.appned(a[:ind[0]-1])
-for i in range(0, k-1):
-    b.append(a[ind[i]+1:ind[i+1]-1])
-if ind[k] != len(a)-1:
-    b.append(a[ind[k]+1:])
-for i in range(len(b)):
-    c.append(min(b[i] * len(b[i])))
-if max(c) > m:
-    m = max(c)
-else:
-    break
-
-print()
+print(ans)
